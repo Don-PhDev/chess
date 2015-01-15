730b71de17487625e374d618ed0608b8af9074b3
Raymond Usbal <raymond@philippinedev.com>
2015-01-15 10:37:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chess
+
+Personal Project: Proof of concept
+![alt
+text](http://i79.photobucket.com/albums/j129/donforrest/ScreenShot2015-01-15at101138AM_zpsd6fc03e6.png
+"Javascript Chess")
+
+### Language: Javascript
+### Library: Angular JS
---
+++ js/immortalgame.js
@@ -0,0 +1,23 @@
+function immortalGame(){
+    return "[Event \"London\"]" +
+        "\n [Site \"London\"]" +
+        "\n [Date \"1851.??.??\"]" +
+        "\n [EventDate \"?\"]" +
+        "\n [Round \"?\"]" +
+        "\n [Result \"1-0\"]" +
+        "\n [White \"Adolf Anderssen\"]" +
+        "\n [Black \"Kieseritzky\"]" +
+        "\n [ECO \"C33\"]" +
+        "\n [WhiteElo \"?\"]" +
+        "\n [BlackElo \"?\"]" +
+        "\n [PlyCount \"45\"]" +
+        "\n " +
+        "\n 1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6" +
+        "\n 7.d3 Nh5 8.Nh4 Qg5 9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6" +
+        "\n 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6 16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6" +
+        "\n Bxg1 {It is from this move that Black's defeat stems. Wilhelm" +
+        "\n Steinitz suggested in 1879 that a better move would be" +
+        "\n 18... Qxa1+; likely moves to follow are 19. Ke2 Qb2 20. Kd2" +
+        "\n Bxg1.} 19. e5 Qxa1+ 20. Ke2 Na6 21.Nxg7+ Kd8 22.Qf6+ Nxf6" +
+        "\n 23.Be7# 1-0";
+}
